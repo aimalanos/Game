7fea6b8c892ce19bc6971138ae17509e4d488d89
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-15 14:57:43 -0800

    Update World.py

--- World.py
+++ World.py
@@ -2,5 +2,10 @@ class World:
     def __init__(self):
         self.turn_count = 0
         self.weather = None
+        self.player = None
+    def add_player(self, player):
+        self.player = player
     def update(self):
-        pass
+        self.player.update()
+        self.turn_count += 1
+        # Weather may need to change
