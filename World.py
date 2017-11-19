4f94f0342329e9de7170dd7ecb5b97dc92fb8e6f
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-19 13:23:35 -0800

    Update World.py

--- World.py
+++ World.py
@@ -1,11 +1,30 @@
 class World:
     def __init__(self):
         self.turn_count = 0
-        self.weather = None
+        self.weather = "clear"
         self.player = None
+        self.squares = []
+        self.healthLoss = 0 # Determines how much health is lost when updating
+        self.hungerLoss = 3 # Determines how much the player's hunger increases when updating
+        self.speedPenalty = 0 # The penalties will be applied depending on the weather
+        self.sociabilityPenalty = 0
     def add_player(self, player):
         self.player = player
+    def reset(self):
+        self.healthLoss = 0
+        self.hungerLoss = 3
+        self.speedPenalty = 0
+        self.sociabilityPenalty = 0
     def update(self):
         self.player.update()
         self.turn_count += 1
-        # Weather may need to change
+        if turn_count % 5 = 0:
+            self.weather = random.choice("clear", "rainy", "hailing", "snowy", "drought")
+            if self.weather == "rainy":
+                self.speedPenalty = self.player.speed // 10
+            elif self.weather == "hailing":
+                self.healthLoss = self.player.health // 15
+            elif self.weather == "snowy":
+                self.sociabilityPenalty = self.player.sociability // 10
+            elif self.weather = "drought":
+                self.hungerLoss = 6
