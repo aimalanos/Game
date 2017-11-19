9772deb74498b7b970b7c7b786a9af3f9a92bb5d
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-19 12:53:53 -0800

    Create Square.py

---
+++ Square.py
@@ -0,0 +1,22 @@
+# I'm calling the rooms "squares" because they aren't proper rooms.
+
+class Square:
+    def __init__(self, w, xcoor, ycoor):
+        self.world = w
+        self.coordinates = (xcoor,ycoor)
+        self.exits = {}
+        for squ in self.world.squares:
+            if squ.coordinates[1] == self.coordinates[1]: # To be east-west adjacent, they must have the same y-coordinate
+                if squ.coordinates[0] == self.coordinates[0] - 1:
+                    self.exits[east] = squ
+                elif squ.coordinates[0] == self.coordinates[0] + 1:
+                    self.exits[west] = squ
+            elif squ.coordinates[0] == self.coordinates[0]: # To be north-south adjacent, they must have the same x-coordinate
+                if squ.coordinates[1] == self.coordinates[1] - 1:
+                    self.exits[south] = squ
+                elif squ.coordinates[1] == self.coordinates[1] + 1:
+                    self.exits[north] = squ    
+        self.creatures = None # I think there should be a max of one NPC per square
+        self.food = 0 # I think different squares should have different quantities of food
+        self.items = []
+    
