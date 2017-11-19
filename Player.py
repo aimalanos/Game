7952bb4d6a3a3609b2a2ea291896f541b0183a38
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-19 13:23:40 -0800

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,12 +1,15 @@
+import random
+
 class Player:
     def __init__(self, w):
         self.name = input("What is your creature's name? ")
-        self.diet = input("Is your creature a carnivore or an herbivore? ")
+        self.diet = input("Is your creature a carnivore or an herbivore? ").lower()
         w.add_player(self)
-        self.world = w      # Still unsure about implementation of the map
-        self.location = 0.0
-        self.hunger = 100
-        self.health = 10
+        self.world = w
+        self.location = random.choice(self.word.squares) # If we want the player to start at a random location on the map.
+        self.home = self.location # The player's home base will be their starting location.
+        self.hunger = 100 # If self.hunger reaches 0, the player's health will decrease at each update
+        self.health = 15
         self.strength = 5
         self.sociability = 5
         self.speed = 5
@@ -14,3 +17,11 @@ class Player:
         self.experience = 0
         self.abilities = []
         self.inventory = []
+    def update(self):
+        self.health -= self.world.healthLoss
+        if self.hunger == 0:
+            self.health -= self.health // 10
+        else:
+            self.hunger -= self.world.hungerLoss
+            if self.hunger < 0:
+                self.hunger = 0
