fb360c3da50bcf350c9a6e4590c7d08dc2a94fc5
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-19 18:20:29 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,8 +4,8 @@ from Player import Player
 from Creature import Creature
 
 w = World()
-for i in range(-10,11):
-    for j in range(-10, 11):
+for i in range(-8,9):
+    for j in range(-8, 9):
         Square(w, i, j)
 p = Player()
 w.player = p
