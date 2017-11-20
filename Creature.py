04b7794379a78f68db8118d82e426f86e15c0529
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-19 18:37:13 -0800

    Update Creature.py

--- Creature.py
+++ Creature.py
@@ -5,6 +5,6 @@ class Creature:
         self.world = w
         self.health = random.randint(level*(10-3), level*(10+3))
         self.strength = random.randint(level*(5-2), level*(5+2))
-        self.sociability = random.randint(level*(5-2), level*(5+2))
+        self.hostility = random.randint(level*(5-2), level*(5+2))
         self.speed = random.randint(level*(5-2), level*(5+2))
         self.experience = (self.health + self.strength + self.sociability + self.speed) // 4
