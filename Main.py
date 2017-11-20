24b0d7c977935b22ea4dce4d30e4c1a81106801f
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-19 18:49:35 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,10 +2,26 @@ from World import World
 from Square import Square
 from Player import Player
 from Creature import Creature
+import random
 
 w = World()
 for i in range(-8,9):
     for j in range(-8, 9):
         Square(w, i, j)
 p = Player()
+for i in range(0,80):
+    r = random.choice(w.squares)
+    if not r.creature:
+        r.creature = Creature(r, 1)
+for i in range(0,80):
+    r = random.choice(w.squares)
+    if not r.creature:
+        r.creature = Creature(r, 2)
+for i in range(0,80):
+    r = random.choice(w.squares)
+    if not r.creature:
+        r.creature = Creature(r, 2)
+for i in range(0,200):
+    r = random.choice(w.squares)
+    r.fruit += 1
 w.player = p
