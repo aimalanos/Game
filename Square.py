be6a6d87d39b68c59cc67a8f675b58b6da19f846
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-11-21 00:47:38 -0800

    Update Square.py

--- Square.py
+++ Square.py
@@ -4,7 +4,7 @@ class Square:
     def __init__(self, w, xcoor, ycoor):
         self.world = w
         self.world.squares.append(self)
-        self.coordinates = (xcoor,ycoor)
+        self.coordinates = [xcoor,ycoor] #changed this to a list instead of (), hope that's chill
         self.exits = {}
         #put the for loop into World.py   
         self.creature = None # I think there should be a max of one NPC per square
