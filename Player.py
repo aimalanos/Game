07fafe688046daa76ab92ee50839ebe1f367ec6b
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-11-20 22:42:07 -0800

    Update Player.py

--- Player.py
+++ Player.py
@@ -11,21 +11,45 @@ class Player:
         self.hunger = 100 # If self.hunger reaches 0, the player's health will decrease at each update
         self.maxHealth = 15
         self.health = 15
+        self.tophealth = self.health
         self.strength = 5
+        self.topstrength = self.strength
         self.sociability = 5
+        self.topsociability =  self.sociability
         self.speed = 5
+        self.topspeed = self.speed
         self.intelligence = 0
+        self.topintelligence = self.intelligence
         self.experience = 0
         self.abilities = []
         self.inventory = []
     def update(self):
         self.health -= self.world.healthLoss
+        if health == 0 or health < 0:
+            self.die()
         if self.hunger == 0:
             self.health -= self.health // 10
         else:
-            self.hunger -= self.world.hungerLoss
+            r = random.randint(0,4) #player will randomly takes to health, strength, sociability, speed, or intelligence
+                if r == 0:
+                    self.hunger -= self.world.hungerLoss
+                elif r == 1:
+                    self.strength -= self.strength/10
+                elif r == 2:
+                    self.sociability -= self.sociability/10
+                elif r == 3:
+                    self.speed -= self.speed/10
             if self.hunger < 0:
                 self.hunger = 0
+                r = random.randint(0,4) #player will randomly takes to health, strength, sociability, speed, or intelligence
+                if r == 0:
+                elif r == 1:
+                    
+    def die(self):
+        w.gameOver()
+    def eat(self):
+    def drink(self): #did we decide to do this or nah? could be interesting when we implement aquatic skills and stuff
+        
 def stats(self):
         print("You are a " + self.diet)
         print("Your location is " + str(self.location))
