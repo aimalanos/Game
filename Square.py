3f3e8a7c032bb211046a5a77fc9a44ca588faece
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-11-21 00:44:31 -0800

    Update Square.py

--- Square.py
+++ Square.py
@@ -5,6 +5,7 @@ class Square:
         self.world = w
         self.world.squares.append(self)
         self.coordinates = (xcoor,ycoor)
+        self.exits = {}
         #put the for loop into World.py   
         self.creature = None # I think there should be a max of one NPC per square
         self.fruit = 0 # I think different squares should have different quantities of food
