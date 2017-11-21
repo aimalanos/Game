5e07a62cfd3e9ab61e91d27e431aa1693dc880a7
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-11-21 00:43:32 -0800

    Update Square.py

--- Square.py
+++ Square.py
@@ -5,18 +5,7 @@ class Square:
         self.world = w
         self.world.squares.append(self)
         self.coordinates = (xcoor,ycoor)
-        self.exits = {}
-        for squ in self.world.squares:
-            if squ.coordinates[1] == self.coordinates[1]: # To be east-west adjacent, they must have the same y-coordinate
-                if squ.coordinates[0] == self.coordinates[0] - 1:
-                    self.exits[east] = squ
-                elif squ.coordinates[0] == self.coordinates[0] + 1:
-                    self.exits[west] = squ
-            elif squ.coordinates[0] == self.coordinates[0]: # To be north-south adjacent, they must have the same x-coordinate
-                if squ.coordinates[1] == self.coordinates[1] - 1:
-                    self.exits[south] = squ
-                elif squ.coordinates[1] == self.coordinates[1] + 1:
-                    self.exits[north] = squ    
+        #put the for loop into World.py   
         self.creature = None # I think there should be a max of one NPC per square
         self.fruit = 0 # I think different squares should have different quantities of food
         self.items = []
