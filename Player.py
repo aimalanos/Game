52b2d133ba7bff8e0bfa6ee7306bfb6c9a74937a
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-21 22:57:05 -0800

    Update Player.py

--- Player.py
+++ Player.py
@@ -3,14 +3,15 @@ import random
 class Player:
     def __init__(self, w):
         self.name = input("What is your creature's name? ")
-        self.diet = input("Is your creature a carnivore or an herbivore? ").lower() #what does .lower() do?
+        self.diet = input("Is your creature a carnivore or an herbivore? ").lower() # lower() puts it in lowercase, which eliminates the problem of whether the player types with capital or lowercase letters
         w.add_player(self)
         self.world = w
-        self.location = random.choice(self.world.squares) # If we want the player to start at a random location on the map.
+        self.location = random.choice(self.world.squares)
         self.home = self.location # The player's home base will be their starting location.
-        self.hunger = 100 # If self.hunger reaches 0, the player's health will decrease at each update
+        self.alive = True
+        self.hunger = 100 # If self.hunger reaches 0, the player's health will decrease at each update.
         self.maxHealth = 15
-        self.health = 15
+        self.health = maxHealth
         self.strength = 5
         self.maxStrength = self.strength
         self.sociability = 5
@@ -18,23 +19,22 @@ class Player:
         self.speed = 5
         self.maxSpeed = self.speed
         self.intelligence = 0
-        self.maxIntelligence = self.intelligence
         self.experience = 0
         self.abilities = []
         self.inventory = []
         self.availabledirs = []
-        for elem in self.location.exits:
-            if elem != None:
-                self.availabledirs.append(elem)
-        self.alive = True
+        for exit in self.location.exits:
+            if exit != None:
+                self.availabledirs.append(exit)
+                
     def update(self):
         self.health -= self.world.healthLoss
         if health <= 0:
             self.die()
-        if self.hunger == 0:
-            self.health -= self.health // 10
-        else:
-            r = random.randint(0,4) #player will randomly take damage to health, strength, sociability, speed, or intelligence
+        if self.hunger > 0:
+            self.hunger -= self.world.hungerLoss
+        elif self.hunger == 0:
+            r = random.randint(0,3) #player will randomly take damage to health, strength, sociability, speed, or intelligence
             if r == 0:
                 self.health -= self.health/10
             elif r == 1:
@@ -43,22 +43,25 @@ class Player:
                 self.sociability -= self.sociability/10
             elif r == 3:
                 self.speed -= self.speed/10
-            elif r == 4:
-                self.intelligence -= self.intelligence/10
-            if self.hunger < 0:
-                self.hunger = 0
+        if self.hunger < 0:
+            self.hunger = 0
+            
     def fillStats(self):
         self.health = self.maxHealth
         self.strength = self.maxStrength
-        self.sociability = self.sociability
+        self.sociability = self.maxSociability
         self.speed = self.maxSpeed
+        
     def die(self):
-        w.gameOver()
+        world.gameOver()
         self.alive = False
+        
     def eat(self):
         self.fillStats()
         self.hunger += 25
+        
     #def drink(self): #did we decide to do this or nah? could be interesting when we implement aquatic skills and stuff
+    
     def north(self):
         if self.location.exits['north'] != None:
             self.location = self.location.exits['north']
