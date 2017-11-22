cea41731f5228c6108b3ff406bbdbd6f8787a8f7
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-11-22 15:06:14 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -175,7 +175,7 @@ def evolve():
 
 while playing and p.alive:
     w.update()
-    command = input("You are at " + str(p.location.coordinates) + "\nWhat would you like to do next? \n help \n me \n go " + str(p.availabledirs) + " \n inventory \n").lower()
+    command = input("You are at " + str(p.location.coordinates) + "\n What would you like to do next? \n help \n me \n go " + str(p.availabledirs) + " \n inventory \n").lower()
     if command == 'help':
         help()
     elif command == 'me':
