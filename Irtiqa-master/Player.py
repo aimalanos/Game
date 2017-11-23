b140058633cb6be3fac1f5f2d5d457afd3b17f32
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-11-22 16:16:15 -0800

    Add files via upload

---
+++ Irtiqa-master/Player.py
@@ -0,0 +1,99 @@
+import random
+
+class Player:
+    def __init__(self, w):
+        self.name = input("What is your creature's name? ")
+        self.diet = input("Is your creature a carnivore or an herbivore? ").lower() # lower() puts it in lowercase, which eliminates the problem of whether the player types with capital or lowercase letters
+        w.add_player(self)
+        self.world = w
+        self.location = random.choice(self.world.squares)
+        self.home = self.location # The player's home base will be their starting location.
+        self.alive = True
+        self.hunger = 100 # If self.hunger reaches 0, the player's health will decrease at each update.
+        self.maxHealth, self.health = 15, 15
+        self.maxStrength, self.strength = 5, 5
+        self.maxSociability, self.sociability = 5, 5
+        self.maxSpeed, self.speed = 5, 5
+        self.intelligence = 0
+        self.experience = 0
+        self.abilities = []
+        self.inventory = []
+        self.availabledirs = []
+        self.defeated = 0 # to keep track of the number of enemies the player has defeated
+        self.allies = 0
+                
+    def update(self):
+        self.health -= self.world.healthLoss
+        if self.health <= 0:
+            self.die()
+        if self.hunger > 0:
+            self.hunger -= self.world.hungerLoss
+        elif self.hunger == 0:
+            r = random.randint(0,3) #player will randomly take damage to health, strength, sociability, speed, or intelligence
+            if r == 0:
+                self.health -= self.health/10
+            elif r == 1:
+                self.strength -= self.strength/10
+            elif r == 2:
+                self.sociability -= self.sociability/10
+            elif r == 3:
+                self.speed -= self.speed/10
+        if self.hunger < 0:
+            self.hunger = 0
+        self.availabledirs = []
+        for exit in self.location.exits:
+            if exit != None:
+                self.availabledirs.append(exit)
+            
+    def fillStats(self):
+        self.health = self.maxHealth
+        self.strength = self.maxStrength
+        self.sociability = self.maxSociability
+        self.speed = self.maxSpeed
+        
+    def die(self):
+        world.gameOver()
+        self.alive = False
+        
+    def eat(self):
+        self.fillStats()
+        self.hunger += 25
+        
+    #def drink(self): #did we decide to do this or nah? could be interesting when we implement aquatic skills and stuff
+    
+    def north(self):
+        if self.location.exits['north'] != None:
+            self.location = self.location.exits['north']
+        else:
+            print('You may not move North. Try again.')
+    def east(self):
+        if self.location.exits['east'] != None:
+            self.location = self.location.exits['east']
+        else:
+            print('You may not move East. Try again.')
+    def west(self):
+        if self.location.exits['west'] != None:
+            self.location = self.location.exits['west']
+        else:
+            print('You may not move West. Try again.')
+    def south(self):
+        if self.location.exits['south'] != None:
+            self.location = self.location.exits['south']
+        else:
+            print('You may not move South. Try again.')
+        
+    def stats(self):
+            print("You are a " + self.diet)
+            print("Your location is " + str(self.location.coordinates))
+            print("Your home is at " + str(self.home.coordinates))
+            print("Hunger = " + str(self.hunger))
+            print("Health = " + str(self.health))
+            print("Strength = " + str(self.strength))
+            print("Sociability = " + str(self.sociability))
+            print("Speed = " + str(self.speed))
+            print("Intelligence = " + str(self.intelligence))
+            print("Experience = " + str(self.experience))
+            print("Abilities = " + str(self.abilities))
+            print("Inventory = " + str(self.inventory) + '\n')
+            print(self.location.exits)
+            print('availabledirs = ' + str(self.availabledirs))
---
+++ Irtiqa-master/World.py
@@ -0,0 +1,52 @@
+from Square import Square
+import random
+
+class World:
+    def __init__(self):
+        self.turn_count = 0
+        self.weather = "clear"
+        self.player = None
+        self.squares = []
+        self.healthLoss = 0 # Determines how much health is lost when updating
+        self.hungerLoss = 3 # Determines how much the player's hunger increases when updating
+        self.speedPenalty = 0 # The penalties will be applied depending on the weather
+        self.sociabilityPenalty = 0
+    def makeMap(self,x,y):
+        for num in range(-x,x): #draw the grid
+            for nums in range(-y,y):
+                self.squares.append(Square(self,num,nums))
+            for squ in self.squares: #assign squares' exits
+                for nei in self.squares:
+                    if squ.coordinates[1] == nei.coordinates[1]: # To be east-west adjacent, they must have the same y-coordinate
+                        if squ.coordinates[0] == nei.coordinates[0] - 1:
+                            nei.exits['west'] = squ
+                        elif squ.coordinates[0] == nei.coordinates[0] + 1:
+                            nei.exits['west'] = squ
+                    elif squ.coordinates[0] == nei.coordinates[0]: # To be north-south adjacent, they must have the same x-coordinate
+                        if squ.coordinates[1] == nei.coordinates[1] - 1:
+                            nei.exits['south'] = squ
+                        elif squ.coordinates[1] == nei.coordinates[1] + 1:
+                            nei.exits['north'] = squ
+
+    def add_player(self, player):
+        self.player = player
+    def reset(self):
+        self.healthLoss = 0
+        self.hungerLoss = 3
+        self.speedPenalty = 0
+        self.sociabilityPenalty = 0
+    def gameOver(self):
+        self.player = None
+    def update(self):
+        self.player.update()
+        self.turn_count += 1
+        if self.turn_count % 5 == 0:
+            self.weather = random.choice("clear", "rainy", "hailing", "snowy", "drought")
+            if self.weather == "rainy":
+                self.speedPenalty = self.player.speed // 10
+            elif self.weather == "hailing":
+                self.healthLoss = self.player.health // 15 #wow this is savage
+            elif self.weather == "snowy":
+                self.sociabilityPenalty = self.player.sociability // 10
+            elif self.weather == "drought":
+                self.hungerLoss = 6
