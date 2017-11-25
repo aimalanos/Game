a2666e75fbfc04dfb97fea507fdcbdbb953f2a66
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-25 12:52:37 -0800

    Update Creature.py

--- Creature.py
+++ Creature.py
@@ -9,4 +9,5 @@ class Creature:
         self.hostility = random.randint(level*(5-2), level*(5+2))
         self.speed = random.randint(level*(5-2), level*(5+2))
         self.fleeRate = random.uniform(0, 0.2)
+        self.allied = False
         self.experience = (self.health + self.strength + self.hostility + self.speed) // 4
