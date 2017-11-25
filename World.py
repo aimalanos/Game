1da5e1066f914eef44be0e767c0bbdcf22a362b1
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-25 12:32:33 -0800

    Update World.py

--- World.py
+++ World.py
@@ -1,6 +1,9 @@
 from Square import Square
 import random
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 class World:
     def __init__(self):
         self.turn_count = 0
@@ -35,6 +38,15 @@ class World:
         self.hungerLoss = 3
         self.speedPenalty = 0
         self.sociabilityPenalty = 0
+    def victory(self):
+        clear()
+        if 'fire' in self.player.inventory:
+            print('You have discovered fire! You have successfully set yourself on the path to civilization! Congratulations!')
+        elif self.player.defeated >= 30:
+            print('You have defeated enough enemies that you are now at the top of the food chain! Congratulations!')
+        elif self.player.allies >= 30:
+            print('You have enough allies to be protected wherever you go! That means you win! Congratulations!')
+        # We should put some fireworks in here
     def gameOver(self):
         self.player = None
     def update(self):
