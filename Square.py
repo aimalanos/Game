d57b2b27c609da7510c4f52430cfd476808637c3
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-11-25 21:52:22 -0800

    Update Square.py

--- Square.py
+++ Square.py
@@ -1,16 +1,9 @@
-# I'm calling the rooms "squares" because they aren't proper rooms.
-
 class Square:
     def __init__(self, w, xcoor, ycoor):
         self.world = w
         self.world.squares.append(self)
         self.coordinates = (xcoor,ycoor) # I had it as parentheses so that it would be a tuple, which is immutable
-        self.exits = {}
-        #self.exits['north'] = None
-        #self.exits['east'] = None
-        #self.exits['west'] = None
-        #self.exits['south'] = None
-        #put the for loop into World.py   
+        self.exits = {'east': None, 'west': None, 'north': None, 'south': None}
         self.creature = None # I think there should be a max of one NPC per square
         self.items = {}
     
