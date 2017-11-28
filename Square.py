e3d57a6861b8e7c5544a6fd1c199d6863c9cd1c9
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-11-28 01:58:31 -0800

    Update Square.py

--- Square.py
+++ Square.py
@@ -2,8 +2,9 @@ class Square:
     def __init__(self, w, xcoor, ycoor):
         self.world = w
         self.world.squares.append(self)
-        self.coordinates = (xcoor,ycoor) # I had it as parentheses so that it would be a tuple, which is immutable
+        self.coordinates = (xcoor,ycoor)
         self.exits = {'east': None, 'west': None, 'north': None, 'south': None}
         self.creature = None # I think there should be a max of one NPC per square
         self.items = {}
-    
+        self.terrain = ''
+        self.weather = ''
