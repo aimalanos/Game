ac28fbaf473ddbedb43aeafd9fc6c63148fd81c1
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-11-29 13:13:03 -0800

    Update Square.py

--- Square.py
+++ Square.py
@@ -8,3 +8,19 @@ class Square:
         self.items = {}
         self.terrain = ''
         self.weather = ''
+
+    def listdirs(self):
+        s = ''
+        l = []
+        for exit in self.exits:
+            if self.exits[exit] != None:
+                l.append(exit)
+                if len(l) == 2:
+                    s = l[0] + " or " + l[1]
+                elif len(l) == 3:
+                    s = l[0] + ", " + l[1] + ", or " + l[2]
+                elif len(l) == 4:
+                    s = l[0] + ", " + l[1] + ", " + l[2] + ", or " + l[3]
+                else:
+                    print('where the dirs at')
+        return s
