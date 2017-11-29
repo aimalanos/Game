df452fc2cc56310d2b96cb797059cc9904878cd9
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-11-28 21:19:48 -0800

    Update Creature.py

--- Creature.py
+++ Creature.py
@@ -4,7 +4,7 @@ import math
 class Creature:
     def __init__(self,square,level):
         self.location = square
-        self.location.creature = True
+        self.level = level
         self.world = self.location.world
         self.health = random.randint(self.level*(10-3), self.level*(10+3))
         self.strength = random.randint(self.level*(5-2), self.level*(5+2))
@@ -12,54 +12,77 @@ class Creature:
         self.speed = random.randint(self.level*(5-2), self.level*(5+2))
         self.fleeRate = random.uniform(0, 0.2)
         self.allied = False
-        self.experience = (self.health + self.strength + self.hostility + self.speed) // 4
-        self.level = level
+        self.experience = (self.health + self.strength + self.hostility + self.speed) 
 
 class Wolf(Creature):
     def __init__(self,square,level):
-        Creature.__init__(square,level)
+        Creature.__init__(self,square,level)
+        self.location = square
+        self.location.creature = self
+        self.level = level
         #self.health = unchanged
         self.strength += 3*self.level
         self.hostility += 3*self.level
         self.speed -= 2*self.level
         self.fleeRate = self.fleeRate/2
+        self.name = 'wolf'
 class Tiger(Creature):
     def __init__(self,square,level):
-        Creature.__init__(square,level)
+        Creature.__init__(self,square,level)
+        self.location = square
+        self.location.creature = self
+        self.level = level
         self.health += 4*self.level
         self.strength += 5*self.level
         self.hostility += 2*self.level
         self.speed += self.level
         self.fleeRate = self.fleeRate
+        self.name = 'tiger'
 class Monkey(Creature):
     def __init__(self,square,level):
-        Creature.__init__(square,level)
+        Creature.__init__(self,square,level)
+        self.location = square
+        self.location.creature = self
+        self.level = level
         self.health -= math.floor(10/level)
         self.strength += 5*self.level
         self.hostility += 3*self.level
         self.speed -= 2*self.level
         self.fleeRate = self.fleeRate/2
+        self.name = 'monkey'
 class Dog(Creature):
     def __init__(self,square,level):
-        Creature.__init__(square,level)
+        Creature.__init__(self,square,level)
+        self.location = square
+        self.location.creature = self
+        self.level = level
         #self.health = unchanged
         self.strength += self.level
         self.hostility += random.randint(1,4)*self.level
         self.speed += self.level
         #self.fleeRate = unchanged
+        self.name = 'dog'
 class Sheep(Creature):
     def __init__(self,square,level):
-        Creature.__init__(square,level)
-        self.health *= 
+        Creature.__init__(self,square,level)
+        self.location = square
+        self.location.creature = self
+        self.level = level
+        self.health = math.floor(self.health*0.5)
         self.strength -= 15
         self.hostility -= 15
         self.speed = 5
         self.fleeRate = 0.8
+        self.name = 'sheep'
 class Snake(Creature):
     def __init__(self,square,level):
-        Creature.__init__(square,level)
+        Creature.__init__(self,square,level)
+        self.location = square
+        self.location.creature = self
+        self.level = level
         self.health = math.ceil(0.3*self.health)
         self.strength += 5*self.level
         self.hostility += 4*self.level
         self.speed *= 5
         self.fleeRate = self.fleeRate/2
+        self.name = 'snake'
