f5546f0f4f6ae856be7aeec339c6845cec82397d
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-11-29 13:13:31 -0800

    Update World.py

--- World.py
+++ World.py
@@ -46,7 +46,7 @@ class World:
         self.player.update()
         self.turn_count += 1
         if self.turn_count % 5 == 0:
-            self.reset()
+            #self.reset()
             self.weather = random.choice(["clear", "rainy", "hailing", "snowy", "drought"]) #remove
             # New creatures will spawn
             randomSquare = random.choice(self.squares)
