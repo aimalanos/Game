2c3a539d30dbdf6add5687771e163f0d8d0ab868
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-12-03 19:53:04 -0800

    Update Player.py

--- Player.py
+++ Player.py
@@ -506,7 +506,7 @@ class Player:
             if 'item' in choice.lower() and len(self.inventory) == 0:
                 print('Your inventory is empty!')
                 choice = input('What will you do? ')
-                        if 'item' in choice.lower() and 'item use' not in self.abilities == 0:
+            if 'item' in choice.lower() and 'item use' not in self.abilities == 0:
                 print('You can\'t do that!')
                 choice = input('What will you do? ')
 
