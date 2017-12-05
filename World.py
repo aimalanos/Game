b63f1bef42c7b98f5e10f6d6f8d5e98dd16d1de6
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-12-04 21:10:46 -0800

    Update World.py

--- World.py
+++ World.py
@@ -7,7 +7,6 @@ import random
 
 class World:
     terrains = ['grassy','grassy','desert','desert','mountainous','mountainous','tundra','forest']
-    weathers = ["clear", "rainy", "hailing", "snowy", "drought"]
     possibleItems = ['stinkfruit', 'sticky sap', 'poison berries', 'big leaf', 'healing salve', 'flowers']
     possibleCreatures = [Creature.Wolf,Creature.Tiger,Creature.Monkey,Creature.Dog,Creature.Sheep,Creature.Snake]
     def __init__(self):
@@ -16,21 +15,21 @@ class World:
         self.player = None
         self.squares = []
         self.possibleCommands = {'me':['me'],'help':['help'],'allstats':['allstats'],'pickup':['pickup'],'go':['go'],'inspect':['inspect'], 'attack': ['attack'], 'befriend': ['befriend'], 'ally': ['ally'], 'dismiss':['dismiss'], 'evolve': ['evolve'], 'use': ['use'], 'inventory': ['inventory'], 'use': ['use'], 'drop': ['drop']}
+        self.weatherlist = ["clear", "rainy", "hailing", "snowy", "drought"]
+        self.weather = random.choice(self.weatherlist)
     def makeMap(self,x,y):
-        for num in range(-x,x): #draw the grid
-            for nums in range(-y,y):
+        for num in range(-x,x+1): #draw the grid
+            for nums in range(-y,y+1):
                 self.squares.append(Square(self,num,nums))
             for squ in self.squares: #assign squares' exits
                 t = random.randint(0,7)
                 squ.terrain = self.terrains[t]
-                w = random.randint(0,4)
-                squ.weather = self.weathers[w]
                 for nei in self.squares:
                     if squ.coordinates[1] == nei.coordinates[1]: # To be east-west adjacent, they must have the same y-coordinate
                         if squ.coordinates[0] == nei.coordinates[0] - 1:
                             nei.exits['west'] = squ
                         elif squ.coordinates[0] == nei.coordinates[0] + 1:
-                            nei.exits['west'] = squ
+                            nei.exits['east'] = squ
                     elif squ.coordinates[0] == nei.coordinates[0]: # To be north-south adjacent, they must have the same x-coordinate
                         if squ.coordinates[1] == nei.coordinates[1] - 1:
                             nei.exits['south'] = squ
@@ -40,8 +39,6 @@ class World:
     def add_player(self, player):
         self.player = player
         
-# gameOver is in Main
-
     def update(self):
         self.player.update()
         self.turn_count += 1
@@ -66,3 +63,5 @@ class World:
                     randomSquare.items[newItem] += 1
                 else:
                     randomSquare.items[newItem] = 1
+            self.weather = random.choice(self.weatherlist)
+            print('here')
