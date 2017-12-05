9ac810bdccee2d57bf30f1d200b6f450e5107bd0
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-12-04 19:51:38 -0800

    Update Square.py

--- Square.py
+++ Square.py
@@ -9,18 +9,35 @@ class Square:
         self.terrain = ''
         self.weather = ''
 
-    def listdirs(self):
-        s = ''
-        l = []
-        for exit in self.exits:
-            if self.exits[exit] != None:
-                l.append(exit)
-                if len(l) == 2:
-                    s = l[0] + " or " + l[1]
-                elif len(l) == 3:
-                    s = l[0] + ", " + l[1] + ", or " + l[2]
-                elif len(l) == 4:
-                    s = l[0] + ", " + l[1] + ", " + l[2] + ", or " + l[3]
+    def availableDirs(self):
+        dirs = ['north','south','east','west']
+        availableDirs = []
+        for direction in dirs:
+            if self.exits[direction] != None:
+                if self.exits[direction].terrain == 'lake':
+                    if 'semiaquatic' in self.abilities:
+                        availableDirs.append(direction)
                 else:
-                    print('where the dirs at')
-        return s
+                    availableDirs.append(direction)
+        print('You may go:')
+        for direction in availableDirs:
+            print('\t' + direction)
+        
+        
+        
+        
+#     def listdirs(self):
+#         s = ''
+#         l = []
+#         for exit in self.exits:
+#             if self.exits[exit] != None:
+#                 l.append(exit)
+#                 if len(l) == 2:
+#                     s = l[0] + " or " + l[1]
+#                 elif len(l) == 3:
+#                     s = l[0] + ", " + l[1] + ", or " + l[2]
+#                 elif len(l) == 4:
+#                     s = l[0] + ", " + l[1] + ", " + l[2] + ", or " + l[3]
+#                 else:
+#                     print('where the dirs at')
+#         return s
