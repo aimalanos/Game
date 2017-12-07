e3be7e7e1ff331bd716962119ec32485f07739c9
thayertoes <33706638+thayertoes@users.noreply.github.com>
2017-12-07 11:43:55 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -562,6 +562,11 @@ while playing and p.alive:
                         if minirow == 4:
                             row -= 1
                             break
+        action = input("Press enter to continue, or \"go ___ \" to move. ")
+            if action == '':
+                pass
+            elif action.split()[0] == 'go':
+                command = action
 
     else:
         print()
