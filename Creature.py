3f9fef8ad9e973bf9133141f0eccfe1b53446061
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-12-08 14:10:52 -0800

    Update Creature.py

--- Creature.py
+++ Creature.py
@@ -8,10 +8,10 @@ class Creature:
         self.originalLocation = self.location
         self.level = level
         self.world = self.location.world
-        self.health = random.randint(self.level*(10-3), self.level*(10+3))
-        self.strength = random.randint(self.level*(5-2), self.level*(5+2))
-        self.hostility = random.randint(self.level*(5-2), self.level*(5+2))
-        self.speed = random.randint(self.level*(5-2), self.level*(5+2))
+        self.health = random.randint(self.level*(7), self.level*(13))
+        self.strength = random.randint(self.level*(3), self.level*(7))
+        self.hostility = random.randint(self.level*(3), self.level*(7))
+        self.speed = random.randint(self.level*(3), self.level*(7))
         self.fleeRate = random.uniform(0, 0.2)
         self.befriended = False
         self.experience = (self.health + self.strength + self.hostility + self.speed) // 3
@@ -22,7 +22,7 @@ class Wolf(Creature):
         self.strength += 3*self.level
         self.hostility += 3*self.level
         self.speed -= 2*self.level
-        if self.speed < 0:
+        if self.speed < 0: # No negative stats
             self.speed = 0
         self.fleeRate = self.fleeRate/2
         self.name = 'wolf'
@@ -55,10 +55,10 @@ class Sheep(Creature):
         Creature.__init__(self,square,level)
         self.health = math.floor(self.health*0.5)
         self.strength -= 15
-        if self.strength < 1:
+        if self.strength < 1: # Strength must be at least 1
             self.strength = 1
         self.hostility -= 15
-        if self.hostility < 1:
+        if self.hostility < 1: # Hostility must be at least 1
             self.hostility = 1
         self.speed = 5
         self.fleeRate = 0.8
@@ -100,5 +100,6 @@ class Leviathan(Creature):
         self.hostility = random.randint(self.level*(2), self.level*(6))
         self.speed = random.randint(self.level*(2), self.level*(6))
         self.fleeRate = 0
+        self.experience = (self.health + self.strength + self.hostility + self.speed) // 2
         self.name = 'leviathan'
         
