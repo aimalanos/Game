02b81ebab110c8709a3323c4c017119f48495dbd
aimalanos <33645253+aimalanos@users.noreply.github.com>
2017-12-08 10:27:32 -0800

    Update World.py

--- World.py
+++ World.py
@@ -3,13 +3,14 @@ import Creature
 import random
 
 class World:
-    terrains = ['forest','desert','desert','hills','hills','lake','tundra','grassy','grassy']
+    terrains = ['forest','desert','desert','hills','hills','lake','tundra','grassy','grassy'] # Some of the terrains are in here twice so that they'll be more likely
     possibleItems = ['sticky sap', 'poison berries', 'big leaf', 'healing salve', 'flowers', 'big stick', 'nesting materials']
     itemWeights = {'fruit': 2, 'meat': 2, 'sticky sap': 3, 'poison berries': 3, 'big leaf': 3, 'healing salve': 3, 'flowers': 2, 'big stick': 3, 'nesting materials': 5}
     possibleCreatures = [Creature.Wolf,Creature.Tiger,Creature.Monkey,Creature.Dog,Creature.Sheep,Creature.Snake]
     creatureNames = ['wolf','tiger','monkey','dog','sheep','snake']
     aquaticCreatures = [Creature.Fish,Creature.Eel,Creature.Leviathan]
     weatherlist = ["clear", "rainy", "hailing", "snowy", "drought"]
+    
     def __init__(self):
         self.turn_count = 0
         self.weather = "clear"
@@ -19,41 +20,43 @@ class World:
         self.weather = 'clear'
         self.mapx = 8
         self.mapy = 8
+        
     def makeMap(self,x,y):
         for i in range(x+1):
             for j in range(y+1):
                 self.squares.append(Square(self, i, j))
         for squ in self.squares:
-             t = random.randint(0,8)
-             squ.terrain = self.terrains[t]
-             if squ.exits['east'] == None:
-                 if squ.coordinates[0] != 2*x:
-                     for squ2 in self.squares:
-                         if squ2.coordinates[0] == squ.coordinates[0] + 1:
-                             if squ2.coordinates[1] == squ.coordinates[1]:
-                                 squ.exits['east'] = squ2
-                                 squ2.exits['west'] = squ
-             if squ.exits['west'] == None:
-                 if squ.coordinates[0] != 0:
-                     for squ2 in self.squares:
-                         if squ2.coordinates[0] == squ.coordinates[0] - 1:
-                             if squ2.coordinates[1] == squ.coordinates[1]:
-                                 squ.exits['west'] = squ2
-                                 squ2.exits['east'] = squ
-             if squ.exits['north'] == None:
-                 if squ.coordinates[1] != 2*y:
-                     for squ2 in self.squares:
-                         if squ2.coordinates[1] == squ.coordinates[1] + 1:
-                             if squ2.coordinates[0] == squ.coordinates[0]:
-                                 squ.exits['north'] = squ2
-                                 squ2.exits['south'] = squ
-             if squ.exits['south'] == None:
-                 if squ.coordinates[0] != 0:
-                     for squ2 in self.squares:
-                         if squ2.coordinates[1] == squ.coordinates[1] - 1:
-                             if squ2.coordinates[0] == squ.coordinates[0]:
-                                 squ.exits['south'] = squ2
-                                 squ2.exits['north'] = squ
+            t = random.randint(0,8)
+            squ.terrain = self.terrains[t] # We randomly assign terrains to squares
+            if squ.exits['east'] == None: # If the square doesn't yet have an east exit, then we add one
+                if squ.coordinates[0] != 2*x: # The eastmost squares won't have an east exit
+                    for squ2 in self.squares:
+                        if squ2.coordinates[0] == squ.coordinates[0] + 1:
+                            if squ2.coordinates[1] == squ.coordinates[1]:
+                                squ.exits['east'] = squ2
+                                squ2.exits['west'] = squ
+            if squ.exits['west'] == None: # If the square doesn't yet have a west exit, then we add one
+                if squ.coordinates[0] != 0: # The westmost squares won't have a west exit
+                    for squ2 in self.squares:
+                        if squ2.coordinates[0] == squ.coordinates[0] - 1:
+                            if squ2.coordinates[1] == squ.coordinates[1]:
+                                squ.exits['west'] = squ2
+                                squ2.exits['east'] = squ
+            if squ.exits['north'] == None: # If the square doesn't yet have a north exit, then we add one
+                if squ.coordinates[1] != 2*y: # The northmost squares won't have a north exit
+                    for squ2 in self.squares:
+                        if squ2.coordinates[1] == squ.coordinates[1] + 1:
+                            if squ2.coordinates[0] == squ.coordinates[0]:
+                                squ.exits['north'] = squ2
+                                squ2.exits['south'] = squ
+            if squ.exits['south'] == None: # If the square doesn't yet have a south exit, then we add one
+                if squ.coordinates[0] != 0: # The southmost squares won't have a south exit
+                    for squ2 in self.squares:
+                        if squ2.coordinates[1] == squ.coordinates[1] - 1:
+                            if squ2.coordinates[0] == squ.coordinates[0]:
+                                squ.exits['south'] = squ2
+                                squ2.exits['north'] = squ
+                                
     def showMap(self):
         sWidth = 8 #square width
         sHeight = 4
@@ -99,20 +102,21 @@ class World:
     def update(self):
         self.player.update()
         self.turn_count += 1
-        if self.turn_count % 5 == 0:
+        if self.turn_count % 5 == 0: # Every five turns...
             # New creatures will spawn
             randomSquare = random.choice(self.squares)
             if randomSquare.creature == None and randomSquare != self.player.home:
                 level = random.randint(1,3)
                 creatureType = random.choice(self.possibleCreatures)
                 creatureType(randomSquare, level)
-            # New items will appear
+            # New food will appear
             for i in range(3):
                 randomSquare = random.choice(self.squares)
                 if 'fruit' in randomSquare.items:
                     randomSquare.items['fruit'] += 1
                 else:
                     randomSquare.items['fruit'] = 1
+            # New items will appear
             for i in range(2):
                 randomSquare = random.choice(self.squares)
                 newItem = random.choice(self.possibleItems)
@@ -120,10 +124,11 @@ class World:
                     randomSquare.items[newItem] += 1
                 else:
                     randomSquare.items[newItem] = 1
+            # The weather will change
             self.weather = random.choice(self.weatherlist)
             if self.weather == 'rainy' or self.weather == 'snowy' or self.weather == 'hailing':
-                if self.player.location.terrain == 'desert':
+                if self.player.location.terrain == 'desert': # It's not going to be rainy, snowy, or hailing in the desert
                     self.weather = clear
-            elif self.weather == 'drought':
+            elif self.weather == 'drought': # There's not going to be a drought in the tundra
                 if self.player.location.terrain == 'tundra':
                     self.weather = 'clear'
